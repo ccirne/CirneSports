9dfff7e910597add905c94a86213a5c27631a1cb
ccirne <claudio.de@oi.com.br>
2016-01-20 00:24:18 -0200

    Commit Inicial

---
+++ CirneSports.LojaVirtual.UnitTest/UnitTestCirneSports.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CirneSports.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestCirneSports
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
