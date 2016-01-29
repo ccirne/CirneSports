56332c41e4f43086d5f3a180fc23fdf1afe03dc2
ccirne <claudio.de@oi.com.br>
2016-01-29 00:24:18 -0200

    Aula 14

--- CirneSports.LojaVirtual.Web/App_Start/RouteConfig.cs
+++ CirneSports.LojaVirtual.Web/App_Start/RouteConfig.cs
@@ -7,44 +7,41 @@ namespace CirneSports.LojaVirtual.Web
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+
+            routes.MapMvcAttributeRoutes();
+
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
 
-            // 1 - Início
+            // 1 - Home
 
-            routes.MapRoute(null,
-                "",
-                new { controller = "Vitrine", 
-                      action = "ListaProdutos", 
-                      categoria = (string)null, 
-                      pagina = 1});
-            
-            // 2
+            routes.MapRoute(null, "", new { controller = "Vitrine", action = "ListaProdutos", categoria = (string)null, pagina = 1 });
 
+
+
+            // 2 - 
             routes.MapRoute(null,
                 "Pagina{pagina}",
-                new { controller = "Vitrine", 
-                      action = "ListaProdutos", 
-                      categoria = (string)null }, 
-                      new { pagina = @"\d+" });
+                new { controller = "Vitrine", action = "ListaProdutos", categoria = (string)null }, new { pagina = @"\d+" });
 
-            // 3
 
             routes.MapRoute(null,
-                "{categoria}",
-                new {controller = "Vitrine", 
-                    action = "ListaProdutos", 
-                    pagina = 1});
+                "{categoria}", new { controller = "Vitrine", action = "ListaProdutos", pagina = 1 });
 
-            // 4
+
 
             routes.MapRoute(null,
-                "{categoria}Pagina{pagina}",
-                new{controller = "Vitrine",
-                    action = "ListaProdutos"},
-                    new { pagina = @"\d+" });
+                "{categoria}/Pagina{pagina}", new { controller = "Vitrine", action = "ListaProdutos" }, new { pagina = @"\d+" });
 
-            routes.MapRoute(null, "{contoller}/{action}");
+
+            //routes.MapRoute(
+            //    "ObterImagem",
+            //    "Vitrine/ObterImagem/{produtoId}",
+            //    new {controller = "Vitrine", action = "ObterImagem", produtoId = UrlParameter.Optional});
+
+            routes.MapRoute(null, "{controller}/{action}");
+
 
         }
     }
--- CirneSports.LojaVirtual.Web/Controllers/VitrineController.cs
+++ CirneSports.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -8,7 +8,7 @@ namespace CirneSports.LojaVirtual.Web.Controllers
     public class VitrineController : Controller
     {
         private ProdutosRepositorio _repositorio;
-        public int ProdutosPorPagina = 10;
+        public int ProdutosPorPagina = 5;
         
         public ViewResult ListaProdutos(string categoria, int pagina = 1)
         {
@@ -18,7 +18,7 @@ namespace CirneSports.LojaVirtual.Web.Controllers
             {
 
                 Produtos = _repositorio.Produtos
-                    .Where(p => p.Categoria == null || p.Categoria == categoria)
+                    .Where(p => categoria == null || p.Categoria == categoria)
                     .OrderBy(p => p.Descricao)
                     .Skip((pagina - 1) * ProdutosPorPagina)
                     .Take(ProdutosPorPagina),
@@ -27,7 +27,7 @@ namespace CirneSports.LojaVirtual.Web.Controllers
                 {
                     PaginaAtual = pagina,
                     ItensPorPagina = ProdutosPorPagina,
-                    ItensTotal = _repositorio.Produtos.Count()
+                    ItensTotal = categoria == null ? _repositorio.Produtos.Count() : _repositorio.Produtos.Count(e => e.Categoria == categoria)
                 },
 
                 CategoriaAtual = categoria
