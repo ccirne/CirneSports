59c2d732dfcc288d10d04b873b2ad2f0aa063288
ccirne <claudio.de@oi.com.br>
2016-01-31 15:09:47 -0200

    Aulas 18 e 19

---
+++ CirneSports.LojaVirtual.Web/Models/CarrinhoViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CirneSports.LojaVirtual.Dominio.Entidade;
+
+namespace CirneSports.LojaVirtual.Web.Models
+{
+    public class CarrinhoViewModel
+    {
+        public Carrinho Carrinho { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
