2209bc244537adcf7e4bc3e25a9717781480306f
ccirne <claudio.de@oi.com.br>
2016-02-02 21:50:37 -0200

    Aula 21

---
+++ CirneSports.LojaVirtual.Dominio/Entidade/Pedido.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CirneSports.LojaVirtual.Dominio.Entidade
+{
+    public class Pedido
+    {
+        [Required(ErrorMessage="Informe seu nome")]
+        public string NomeCliente { get; set; }
+
+        [Required(ErrorMessage = "Informe seu Cep")]
+        [Display(Name = "Cep:")]
+        public string Cep { get; set; }
+
+        [Required(ErrorMessage = "Informe seu endereço")]
+        [Display(Name = "Endereço:")]
+        public string Endereco { get; set; }
+
+        [Display(Name = "Complemento:")]
+        public string Complemento { get; set; }
+
+        [Required(ErrorMessage = "Informe sua cidade")]
+        [Display(Name = "Cidade:")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "Informe seu bairro")]
+        [Display(Name = "Bairro:")]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "Informe seu e-mail")]
+        [Display(Name = "E-mail:")]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        public string Email { get; set; }
+
+        public Boolean EmbrulhaPresente { get; set; }
+    }
+}
