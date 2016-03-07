94d330b6f5bd0c24263a61df3a4891bb9bbdb00e
ccirne <claudio.de@oi.com.br>
2016-03-06 23:02:59 -0300

    Salvo Parcialmente - CRUD Administrativo de Produto

---
+++ CirneSports.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -0,0 +1,22 @@
+﻿using CirneSports.LojaVirtual.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CirneSports.LojaVirtual.Web.Areas.Administrativo.Controllers
+{
+    public class ProdutoController : Controller
+    {
+
+        private ProdutosRepositorio _repositorio;
+
+        public ActionResult Index()
+        {
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos;
+            return View(produtos);
+        }
+    }
+}
