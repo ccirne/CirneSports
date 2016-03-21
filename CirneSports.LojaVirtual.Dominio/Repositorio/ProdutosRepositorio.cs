317dc85d5ef95beb49a06eb13657607797375b07
ccirne <claudio.de@oi.com.br>
2016-03-20 22:08:38 -0300

    Aula 32

--- CirneSports.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
+++ CirneSports.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -12,5 +12,32 @@ namespace CirneSports.LojaVirtual.Dominio.Repositorio
             get { return _context.Produtos; }
         }
 
+        //Salvar produto - Alterar produto
+        public void Salvar(Produto produto)
+        {
+            if (produto.ProdutoId == 0)
+            {
+                //Salvando
+                _context.Produtos.Add(produto);
+            }
+            else
+            {
+                //Alterando
+                Produto prod = _context.Produtos.Find(produto.ProdutoId);
+
+                if (prod != null)
+                {
+                    prod.Nome = produto.Nome;
+                    prod.Descricao = produto.Descricao;
+                    prod.Preco = produto.Preco;
+                    prod.Categoria = produto.Categoria;
+
+                }
+            }
+
+            _context.SaveChanges();
+        }
+
+
     }
 }
--- CirneSports.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
+++ CirneSports.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -29,6 +29,22 @@ namespace CirneSports.LojaVirtual.Web.Areas.Administrativo.Controllers
             return View(produto);
         }
 
+        [HttpPost]
+        public ActionResult Alterar(Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                _repositorio = new ProdutosRepositorio();
+                _repositorio.Salvar(produto);
+
+                TempData["mensagem"] = string.Format("{0} foi salvo com sucesso!", produto.Nome);
+
+                return RedirectToAction("Index");
+            }
+            return View(produto);
+        }
+
+
 
     }
 }
