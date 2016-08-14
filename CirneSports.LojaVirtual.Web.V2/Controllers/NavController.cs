5543bd2942348435d73e65be3a52f2e11ec50d29
ccirne <claudio.de@oi.com.br>
2016-08-13 21:37:25 -0300

    Aula 61

---
+++ CirneSports.LojaVirtual.Web.V2/Controllers/NavController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CirneSports.LojaVirtual.Web.V2.Models;
+
+namespace CirneSports.LojaVirtual.Web.V2.Controllers
+{
+    public class NavController : Controller
+    {
+        // GET: Nav
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("nav/{id}/{marca}")]
+        public ActionResult ObterProdutosPorMarca (string id)
+        {
+            var model = new ProdutosViewModel { Produtos = null };
+
+            return View("Index", model);
+        }
+    }
+}
---
+++ CirneSports.LojaVirtual.Web.V2/Models/ProdutosViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CirneSports.LojaVirtual.Dominio.Entidade.Vitrine;
+
+namespace CirneSports.LojaVirtual.Web.V2.Models
+{
+    public class ProdutosViewModel
+    {
+        public List<ProdutoVitrine> Produtos { get; set; }
+
+    }
+}
