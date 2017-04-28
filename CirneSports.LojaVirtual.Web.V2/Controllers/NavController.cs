162455573fe2fa0483953ac88c208ddbce4f2fa4
ccirne <claudio.de@oi.com.br>
2017-04-28 01:44:22 -0300

    Aula 69

--- CirneSports.LojaVirtual.Web.V2/Controllers/NavController.cs
+++ CirneSports.LojaVirtual.Web.V2/Controllers/NavController.cs
@@ -57,5 +57,38 @@ namespace CirneSports.LojaVirtual.Web.V2.Controllers
 
             return View("Navegacao", _model);
         }
+
+        [Route("nav/esportes/{id}/{esportes}")]
+        public ActionResult ObterProdutosPorCategoria(string id, string esportes)
+        {
+            _repositorio = new ProdutoModeloRepositorio();
+            var produtos = _repositorio.ObterProdutosVitrine(categoria: id);
+
+            _model = new ProdutosViewModel { Produtos = produtos, Titulo = esportes };
+
+            return View("Navegacao", _model);
+        }
+
+        [Route("nav/genero/{idgenero}/{genero}/{idgrupo}/{grupo}")]
+        public ActionResult ObterProdutosCalcadosMasc(string idgenero, string genero, string idgrupo, string grupo)
+        {
+            _repositorio = new ProdutoModeloRepositorio();
+            var produtos = _repositorio.ObterProdutosVitrine(genero: idgenero, grupo: idgrupo);
+
+            _model = new ProdutosViewModel { Produtos = produtos, Titulo = grupo };
+
+            return View("Navegacao", _model);
+        }
+
+        [Route("nav/genero/{idgenero}/{genero}/{idgrupo}/{grupo}/{idsubgrupo}/{subgrupo}")]
+        public ActionResult ObterProdutosCalcadosMascChuteira(string idgenero, string genero, string idgrupo, string grupo, string idsubgrupo, string subgrupo)
+        {
+            _repositorio = new ProdutoModeloRepositorio();
+            var produtos = _repositorio.ObterProdutosVitrine(genero: idgenero, grupo: idgrupo, subgrupo: idsubgrupo);
+
+            _model = new ProdutosViewModel { Produtos = produtos, Titulo = grupo + " / " + subgrupo};
+
+            return View("Navegacao", _model);
+        }
     }
 }
